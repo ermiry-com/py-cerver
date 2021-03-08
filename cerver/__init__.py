6140e7ecd49092aef197be4db298abff4419d645
ermiry <erick.salas@ermiry.com>
2021-03-07 21:36:04 -0600

    Added base main module configuration

---
+++ cerver/__init__.py
@@ -0,0 +1,8 @@
+from .cerver import *
+
+from .http import *
+from .response import *
+from .route import *
+from .status import *
+
+__version__ = "0.1"
---
+++ cerver/lib.py
@@ -0,0 +1,3 @@
+import ctypes
+
+lib = ctypes.CDLL ("./libs/libcerver.so")
