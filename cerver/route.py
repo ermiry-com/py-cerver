3eb11ca016417b614f1895b5460a1f0a8536663a
ermiry <erick.salas@ermiry.com>
2021-03-07 23:03:02 -0600

    Moved request methods types to dedicated sources

--- cerver/route.py
+++ cerver/route.py
@@ -2,14 +2,6 @@ from ctypes import c_int, c_char_p, c_void_p, CFUNCTYPE
 
 from .lib import lib
 
-RequestMethod = c_int
-
-REQUEST_METHOD_DELETE = 0
-REQUEST_METHOD_GET = 1 
-REQUEST_METHOD_HEAD = 2
-REQUEST_METHOD_POST = 3
-REQUEST_METHOD_PUT = 4
-
 HttpHandler = CFUNCTYPE (None, c_void_p, c_void_p)
 
 http_route_create = lib.http_route_create
