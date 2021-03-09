cbce3736d659dd3ba033fa4e788546e8a0d48abc
ermiry <erick.salas@ermiry.com>
2021-03-08 23:49:33 -0600

    Added base HTTP auth methods bindings

--- cerver/http.py
+++ cerver/http.py
@@ -2,6 +2,8 @@ from ctypes import c_void_p, c_char_p
 
 from .lib import lib
 
+from .alg import jwt_alg_t
+
 # main
 http_cerver_get = lib.http_cerver_get
 http_cerver_get.argtypes = [c_void_p]
@@ -19,3 +21,17 @@ http_cerver_static_path_add.restype = c_void_p
 # routes
 http_cerver_route_register = lib.http_cerver_route_register
 http_cerver_route_register.argtypes = [c_void_p, c_void_p]
+
+# auth
+# sets the jwt algorithm used for encoding & decoding jwt tokens
+# the default value is JWT_ALG_HS256
+http_cerver_route_register = lib.http_cerver_route_register
+http_cerver_route_register.argtypes = [c_void_p, jwt_alg_t]
+
+# sets the filename from where the jwt private key will be loaded
+http_cerver_auth_set_jwt_priv_key_filename = lib.http_cerver_auth_set_jwt_priv_key_filename
+http_cerver_auth_set_jwt_priv_key_filename.argtypes = [c_void_p, c_char_p]
+
+# sets the filename from where the jwt public key will be loaded
+http_cerver_auth_set_jwt_pub_key_filename = lib.http_cerver_auth_set_jwt_pub_key_filename
+http_cerver_auth_set_jwt_pub_key_filename.argtypes = [c_void_p, c_char_p]
