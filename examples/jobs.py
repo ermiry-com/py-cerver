a96951a766da2ac7b0262618b86ac4131ca64924
ermiry <erick.salas@ermiry.com>
2021-03-10 14:40:39 -0600

    Base job queue example seems to be working

--- examples/jobs.py
+++ examples/jobs.py
@@ -1,10 +1,12 @@
 import os, signal, sys
+import time
 import ctypes
 
 import cerver
 import cerver.threads
 
 web_cerver = None
+job_queue = None
 
 # end
 def end (signum, frame):
@@ -14,6 +16,11 @@ def end (signum, frame):
 	cerver.cerver_end ()
 	sys.exit ("Done!")
 
+@ctypes.CFUNCTYPE (None, ctypes.c_void_p)
+def custom_handler_method (job_handler):
+	print ("custom_handler_method ()")
+	time.sleep (1)
+
 # GET /
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
 def main_handler (http_receive, request):
@@ -21,8 +28,20 @@ def main_handler (http_receive, request):
 		http_receive, 200, "Main handler!".encode ('utf-8')
 	)
 
+# GET /jobs
+@ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
+def jobs_handler (http_receive, request):
+	global job_queue
+	cerver.threads.job_handler_wait (job_queue, None, None)
+
+	cerver.http_response_json_msg_send (
+		http_receive, 200, "Jobs route works!".encode ('utf-8')
+	)
+
 def start ():
 	global web_cerver
+	global job_queue
+
 	web_cerver = cerver.cerver_create_web (
 		"web-cerver".encode ('utf-8'), 8080, 10
 	)
@@ -40,6 +59,15 @@ def start ():
 	# GET /
 	main_route = cerver.http_route_create (cerver.REQUEST_METHOD_GET, "/".encode ('utf-8'), main_handler)
 	cerver.http_cerver_route_register (http_cerver, main_route)
+
+	# GET /jobs
+	jobs_route = cerver.http_route_create (cerver.REQUEST_METHOD_GET, "jobs".encode ('utf-8'), jobs_handler)
+	cerver.http_cerver_route_register (http_cerver, jobs_route)
+
+	# job queue
+	job_queue = cerver.threads.job_queue_create (cerver.threads.JOB_QUEUE_TYPE_HANDLERS)
+	cerver.threads.job_queue_set_handler (job_queue, custom_handler_method)
+	cerver.threads.job_queue_start (job_queue)
 
 	# start
 	cerver.cerver_start (web_cerver)
