6e9d7a8220d6b6b95a6209c8aea9f1df37bf2e37
ermiry <erick.salas@ermiry.com>
2021-03-20 23:31:38 -0600

    Refactored example user class definition

--- examples/users.py
+++ examples/users.py
@@ -1,12 +1,14 @@
 users = []
 
-class User:
-	def __init__ (self):
-		self.id
-		self.name
-		self.username
-		self.password
-		self.role
+class User ():
+	def __init__ (self, id, iat, name, role, username)
+		self.id = id
+		self.iat = iat
+		self.name = name
+		self.role = role
+		self.username = username
+
+		self.password = None
 
 	def __str__ (self):
 		return f'User: \n\t{self.id} \n\t{self.name} \n\t{self.username} \n\t{self.role}'
