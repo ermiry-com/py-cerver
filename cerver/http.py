9f2303a30f2b1d94695d2287d405580b274c431a
ermiry <erick.salas@ermiry.com>
2021-03-21 22:37:15 -0600

    Added http_cerver_enable_admin_routes () binding

--- cerver/http.py
+++ cerver/http.py
@@ -112,6 +112,10 @@ http_cerver_auth_generate_bearer_jwt_json.restype = c_uint8
 http_cerver_all_stats_print = lib.http_cerver_all_stats_print
 http_cerver_all_stats_print.argtypes = [c_void_p]
 
+# admin
+http_cerver_enable_admin_routes = lib.http_cerver_enable_admin_routes
+http_cerver_enable_admin_routes.argtypes = [c_void_p, c_bool]
+
 # parser
 http_query_pairs_get_value = lib.http_query_pairs_get_value
 http_query_pairs_get_value.argtypes = [c_void_p, c_char_p]
