5bf8552d52e45c7b94ceb4416cd20d055fd0443b
ermiry <erick.salas@ermiry.com>
2021-03-25 11:44:31 -0600

    Added base api integration test cerver

---
+++ test/web/users.py
@@ -0,0 +1,24 @@
+users = []
+
+class User ():
+	def __init__ (self, id, iat, name, role, username):
+		self.id = id
+		self.iat = iat
+		self.name = name
+		self.role = role
+		self.username = username
+
+		self.password = None
+
+	def __str__ (self):
+		return f'User: \n\t{self.id} \n\t{self.name} \n\t{self.username} \n\t{self.role}'
+
+def user_add (user):
+	users.append (user)
+
+def user_get_by_username (username):
+	for u in users:
+		if u.username == username:
+			return u
+	
+	return None
