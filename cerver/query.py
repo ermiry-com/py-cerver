2dc8a6e714f100921bdc14170655698bf1f846d1
Djcharles26 <jlaraanaya@gmail.com>
2021-03-30 21:20:11 -0600

    Created a single module for required query functions

---
+++ cerver/query.py
@@ -0,0 +1,12 @@
+from ctypes import c_void_p, c_char_p, POINTER
+
+from .lib import lib
+
+from .types.string import String
+
+http_query_pairs_get_value = lib.http_query_pairs_get_value
+http_query_pairs_get_value.argtypes = [c_void_p, c_char_p]
+http_query_pairs_get_value.restype = POINTER (String)
+
+http_query_pairs_print = lib.http_query_pairs_print
+http_query_pairs_print.argtypes = [c_void_p]
