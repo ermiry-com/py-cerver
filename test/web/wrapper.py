25e332dde0d801bf503569308579c29e7ac1cf8e
ermiry <erick.salas@ermiry.com>
2021-04-16 13:34:35 -0500

    Updated tests imports to match examples

--- test/web/wrapper.py
+++ test/web/wrapper.py
@@ -3,47 +3,48 @@ import json
 
 import ctypes
 
-import cerver
+from cerver import *
+from cerver.http import *
 
 api_cerver = None
 
 # end
 def end (signum, frame):
-	cerver.http_cerver_all_stats_print (cerver.http_cerver_get (api_cerver))
-	cerver.cerver_teardown (api_cerver)
-	cerver.cerver_end ()
+	http_cerver_all_stats_print (http_cerver_get (api_cerver))
+	cerver_teardown (api_cerver)
+	cerver_end ()
 	sys.exit ("Done!")
 
 # GET /
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
 def main_handler (http_receive, request):
-	cerver.http_send_response (http_receive, cerver.HTTP_STATUS_OK, { "msg": "Wrapper works!" })
+	http_send_response (http_receive, HTTP_STATUS_OK, { "msg": "Wrapper works!" })
 
 # GET /echo
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
 def echo_handler (http_receive, request):
-	body_values = cerver.http_request_get_query_params (request)
-	value = cerver.http_request_get_query_value (body_values, "value")
-	cerver.http_send_response (http_receive, cerver.HTTP_STATUS_OK, { "echo_says": f"Wrapper received: {value}" })
+	body_values = http_request_get_query_params (request)
+	value = http_request_get_query_value (body_values, "value")
+	http_send_response (http_receive, HTTP_STATUS_OK, { "echo_says": f"Wrapper received: {value}" })
 
 # POST /data
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
 def data_handler (http_receive, request):
-	body = cerver.http_request_get_body_json (request)
-	cerver.http_send_response (http_receive, cerver.HTTP_STATUS_OK, { "echo": body })
+	body = http_request_get_body_json (request)
+	http_send_response (http_receive, HTTP_STATUS_OK, { "echo": body })
 
 # POST /token
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
 def token_handler (http_receive, request):
-	body = cerver.http_request_get_body_json (request)
-	cerver.http_jwt_sign_and_send (http_receive, cerver.HTTP_STATUS_OK, body)
+	body = http_request_get_body_json (request)
+	http_jwt_sign_and_send (http_receive, HTTP_STATUS_OK, body)
 
 # GET /auth
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
 def auth_handler (http_receive, request):
-	token_values = cerver.http_jwt_token_decode (request)
-	cerver.http_send_response (
-		http_receive, cerver.HTTP_STATUS_OK, {
+	token_values = http_jwt_token_decode (request)
+	http_send_response (
+		http_receive, HTTP_STATUS_OK, {
 		"msg": "is authenticated",
 		"token_values": token_values
 	})
@@ -51,28 +52,28 @@ def auth_handler (http_receive, request):
 def start ():
 	global api_cerver
 
-	api_cerver = cerver.cerver_main_http_configuration ()
-	http_cerver = cerver.http_cerver_get (api_cerver)
-	cerver.cerver_auth_http_configuration (http_cerver, cerver.JWT_ALG_RS256, "keys/key.key", "keys/key.pub")
+	api_cerver = cerver_main_http_configuration ()
+	http_cerver = http_cerver_get (api_cerver)
+	cerver_auth_http_configuration (http_cerver, JWT_ALG_RS256, "keys/key.key", "keys/key.pub")
 
 	# GET /
-	main_route = cerver.http_create_route (cerver.REQUEST_METHOD_GET, "/", main_handler, http_cerver = http_cerver)
+	main_route = http_create_route (REQUEST_METHOD_GET, "/", main_handler, http_cerver = http_cerver)
 
 	# GET /echo
-	echo_route = cerver.http_create_route (cerver.REQUEST_METHOD_GET, "echo", echo_handler, main_route)
+	echo_route = http_create_route (REQUEST_METHOD_GET, "echo", echo_handler, main_route)
 
 	# POST /data
-	data_route = cerver.http_create_route (cerver.REQUEST_METHOD_POST, "data", data_handler, main_route)
+	data_route = http_create_route (REQUEST_METHOD_POST, "data", data_handler, main_route)
 
 	# POST /token
-	token_route = cerver.http_create_route (cerver.REQUEST_METHOD_POST, "token", token_handler, main_route)
+	token_route = http_create_route (REQUEST_METHOD_POST, "token", token_handler, main_route)
 
 	# GET /auth
-	auth_route = cerver.http_create_secure_route (cerver.REQUEST_METHOD_GET, "auth", auth_handler, main_route)
+	auth_route = http_create_secure_route (REQUEST_METHOD_GET, "auth", auth_handler, main_route)
 
-	cerver.cerver_start (api_cerver)
+	cerver_start (api_cerver)
 
 if __name__ == "__main__":
-	cerver.cerver_initialize(end, True)
+	cerver_initialize(end, True)
 
 	start()
