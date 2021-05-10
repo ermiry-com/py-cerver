f5cc349215950df328d14bac7716ba012aa60072
ermiry <erick.salas@ermiry.com>
2021-05-09 23:02:21 -0500

    Refactored http_route_create () definition

--- cerver/http/route.py
+++ cerver/http/route.py
@@ -21,7 +21,7 @@ HttpDecodeData = CFUNCTYPE (c_void_p, c_void_p)
 HttpDeleteDecoded = CFUNCTYPE (None, c_void_p)
 
 http_route_create = lib.http_route_create
-http_route_create.argtypes = [c_int, c_char_p, HttpHandler]
+http_route_create.argtypes = [RequestMethod, c_char_p, HttpHandler]
 http_route_create.restype = c_void_p
 
 http_route_set_handler = lib.http_route_set_handler
