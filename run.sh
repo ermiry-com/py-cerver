f8a70968ac1e2b85f99b33fdf3124cd3b1af11f5
ermiry <erick.salas@ermiry.com>
2021-06-28 08:30:14 -0500

    Handling multiple integration test in run script
    Also added new logs to indicate when a new test will begin

--- run.sh
+++ run.sh
@@ -10,9 +10,11 @@ sudo docker kill $(sudo docker ps -q)
 make TYPE=test -j4 test || { exit 1; }
 
 # compile docker
+echo "Building test docker image..."
 sudo docker build -t ermiry/pycerver:test -f Dockerfile.test . || { exit 1; }
 
 # ping
+echo "Ping integration test..."
 sudo docker run \
 	-d \
 	--name test --rm \
@@ -28,6 +30,7 @@ sudo docker inspect test --format='{{.State.ExitCode}}' || { exit 1; }
 sudo docker kill $(sudo docker ps -q)
 
 # web
+echo "Web integration test..."
 sudo docker run \
 	-d \
 	--name test --rm \
@@ -43,6 +46,7 @@ sudo docker inspect test --format='{{.State.ExitCode}}' || { exit 1; }
 sudo docker kill $(sudo docker ps -q)
 
 # api
+echo "API integration test..."
 sudo docker run \
 	-d \
 	--name test --rm \
@@ -58,6 +62,7 @@ sudo docker inspect test --format='{{.State.ExitCode}}' || { exit 1; }
 sudo docker kill $(sudo docker ps -q)
 
 # upload
+echo "Upload integration test..."
 sudo docker run \
 	-d \
 	--name test --rm \
@@ -72,7 +77,24 @@ sudo docker inspect test --format='{{.State.ExitCode}}' || { exit 1; }
 
 sudo docker kill $(sudo docker ps -q)
 
+# multiple
+echo "Multiple integration test..."
+sudo docker run \
+	-d \
+	--name test --rm \
+	-p 8080:8080 \
+	ermiry/pycerver:test python3 web/multiple.py
+
+sleep 2
+
+sudo docker inspect test --format='{{.State.ExitCode}}' || { exit 1; }
+
+./test/bin/client/web/multiple || { exit 1; }
+
+sudo docker kill $(sudo docker ps -q)
+
 # jobs
+echo "Jobs integration test..."
 sudo docker run \
 	-d \
 	--name test --rm \
@@ -88,6 +110,7 @@ sudo docker inspect test --format='{{.State.ExitCode}}' || { exit 1; }
 sudo docker kill $(sudo docker ps -q)
 
 # admin
+echo "Admin integration test..."
 sudo docker run \
 	-d \
 	--name test --rm \
@@ -103,6 +126,7 @@ sudo docker inspect test --format='{{.State.ExitCode}}' || { exit 1; }
 sudo docker kill $(sudo docker ps -q)
 
 # wrapper
+echo "Wrapper integration test..."
 sudo docker run \
 	-d \
 	--name test --rm \
@@ -116,3 +140,5 @@ sudo docker inspect test --format='{{.State.ExitCode}}' || { exit 1; }
 ./test/bin/client/web/wrapper || { exit 1; }
 
 sudo docker kill $(sudo docker ps -q)
+
+printf "\n\nDone\n\n"
