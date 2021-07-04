e30767ab7fa20dcef8ea649e0b2186965b89d547
ermiry <erick.salas@ermiry.com>
2021-07-03 20:14:51 -0500

    Added methods to move multi-part files

--- cerver/http/multipart.py
+++ cerver/http/multipart.py
@@ -1,4 +1,4 @@
-from ctypes import c_int, c_uint32, c_void_p, c_char_p, c_bool, POINTER
+from ctypes import c_int, c_uint, c_uint32, c_void_p, c_char_p, c_bool, POINTER
 
 from ..lib import lib
 
@@ -44,14 +44,6 @@ http_multi_part_get_generated_filename_len.restype = c_int
 
 http_multi_part_set_generated_filename = lib.http_multi_part_set_generated_filename
 
-http_multi_part_get_saved_filename = lib.http_multi_part_get_saved_filename
-http_multi_part_get_saved_filename.argtypes = [c_void_p]
-http_multi_part_get_saved_filename.restype = c_char_p
-
-http_multi_part_get_saved_filename_len = lib.http_multi_part_get_saved_filename_len
-http_multi_part_get_saved_filename_len.argtypes = [c_void_p]
-http_multi_part_get_saved_filename_len.restype = c_int
-
 http_multi_part_get_n_reads = lib.http_multi_part_get_n_reads
 http_multi_part_get_n_reads.argtypes = [c_void_p]
 http_multi_part_get_n_reads.restype = c_uint32
@@ -60,6 +52,18 @@ http_multi_part_get_total_wrote = lib.http_multi_part_get_total_wrote
 http_multi_part_get_total_wrote.argtypes = [c_void_p]
 http_multi_part_get_total_wrote.restype = c_uint32
 
+http_multi_part_get_saved_filename = lib.http_multi_part_get_saved_filename
+http_multi_part_get_saved_filename.argtypes = [c_void_p]
+http_multi_part_get_saved_filename.restype = c_char_p
+
+http_multi_part_get_saved_filename_len = lib.http_multi_part_get_saved_filename_len
+http_multi_part_get_saved_filename_len.argtypes = [c_void_p]
+http_multi_part_get_saved_filename_len.restype = c_int
+
+http_multi_part_get_moved_file = lib.http_multi_part_get_moved_file
+http_multi_part_get_moved_file.argtypes = [c_void_p]
+http_multi_part_get_moved_file.restype = c_bool
+
 http_multi_part_get_value = lib.http_multi_part_get_value
 http_multi_part_get_value.argtypes = [c_void_p]
 http_multi_part_get_value.restype = c_char_p
@@ -67,6 +71,13 @@ http_multi_part_get_value.restype = c_char_p
 http_multi_part_get_value_len = lib.http_multi_part_get_value_len
 http_multi_part_get_value_len.argtypes = [c_void_p]
 http_multi_part_get_value_len.restype = c_int
+
+http_multi_part_move_file = lib.http_multi_part_move_file
+http_multi_part_move_file.argtypes = [c_void_p, c_char_p]
+http_multi_part_move_file.restype = c_uint
+
+http_multi_part_headers_print = lib.http_multi_part_headers_print
+http_multi_part_headers_print.argtypes = [c_void_p]
 
 http_multi_part_print = lib.http_multi_part_print
 http_multi_part_print.argtypes = [c_void_p]
