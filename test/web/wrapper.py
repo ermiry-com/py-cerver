e6c53ef36382596affe4407e1cbe5b9e505d38f5
ermiry <erick.salas@ermiry.com>
2021-10-26 19:27:51 -0500

    Updated wrapper integration with latest methods

--- test/web/wrapper.py
+++ test/web/wrapper.py
@@ -6,12 +6,12 @@ import ctypes
 from cerver import *
 from cerver.http import *
 
-api_cerver = None
+web_service = None
 
 # end
 def end (signum, frame):
-	http_cerver_all_stats_print (http_cerver_get (api_cerver))
-	cerver_teardown (api_cerver)
+	http_cerver_all_stats_print (http_cerver_get (web_service))
+	cerver_teardown (web_service)
 	cerver_end ()
 	sys.exit ("Done!")
 
@@ -19,7 +19,7 @@ def end (signum, frame):
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
 def main_handler (http_receive, request):
 	http_send_response (
-		http_receive, HTTP_STATUS_OK, { "msg": "Wrapper works!" }
+		http_receive, HTTP_STATUS_OK, { "oki": "doki!" }
 	)
 
 # GET /echo
@@ -29,7 +29,7 @@ def echo_handler (http_receive, request):
 	value = http_request_get_query_value (body_values, "value")
 	http_send_response (
 		http_receive, HTTP_STATUS_OK,
-		{ "echo_says": f"Wrapper received: {value}" }
+		{ "echo": f"Values received: {value}" }
 	)
 
 # POST /data
@@ -42,7 +42,7 @@ def data_handler (http_receive, request):
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
 def token_handler (http_receive, request):
 	body = http_request_get_body_json (request)
-	http_jwt_sign_and_send (http_receive, HTTP_STATUS_OK, body)
+	http_jwt_create_and_send (http_receive, HTTP_STATUS_OK, body)
 
 # GET /auth
 @ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
@@ -50,48 +50,79 @@ def auth_handler (http_receive, request):
 	token_values = http_jwt_token_decode (request)
 	http_send_response (
 		http_receive, HTTP_STATUS_OK, {
-		"msg": "is authenticated",
-		"token_values": token_values
+		"oki": "doki",
+		"values": token_values
 	})
 
+# GET /parent
+@ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
+def parent_handler (http_receive, request):
+	pass
+
+# GET /child
+@ctypes.CFUNCTYPE (None, ctypes.c_void_p, ctypes.c_void_p)
+def child_handler (http_receive, request):
+	pass
+
 def start ():
-	global api_cerver
+	global web_service
 
-	api_cerver = cerver_main_http_configuration ()
-	http_cerver = http_cerver_get (api_cerver)
+	web_service = http_cerver_configuration ("web")
+	http_cerver = http_cerver_get (web_service)
 
-	cerver_auth_http_configuration (
+	http_cerver_auth_configuration (
 		http_cerver, JWT_ALG_RS256, "./web/keys/key.key", "./web/keys/key.pub"
 	)
 
 	# GET /
 	main_route = http_create_route (
-		REQUEST_METHOD_GET, "/", main_handler, http_cerver = http_cerver
+		http_cerver, REQUEST_METHOD_GET, "/", main_handler
 	)
 
 	# GET /echo
 	echo_route = http_create_route (
-		REQUEST_METHOD_GET, "echo", echo_handler, main_route
+		http_cerver, REQUEST_METHOD_GET, "echo", echo_handler
 	)
 
 	# POST /data
 	data_route = http_create_route (
-		REQUEST_METHOD_POST, "data", data_handler, main_route
+		http_cerver, REQUEST_METHOD_POST, "data", data_handler
 	)
 
 	# POST /token
 	token_route = http_create_route (
-		REQUEST_METHOD_POST, "token", token_handler, main_route
+		http_cerver, REQUEST_METHOD_POST, "token", token_handler
 	)
 
 	# GET /auth
-	auth_route = http_create_secure_route (
-		REQUEST_METHOD_GET, "auth", auth_handler, main_route
+	auth_route = http_create_route (
+		http_cerver, REQUEST_METHOD_GET, "auth",
+		auth_handler, HTTP_ROUTE_AUTH_TYPE_BEARER
 	)
 
-	cerver_start (api_cerver)
+	# GET /parent
+	parent_route = http_create_route (
+		http_cerver, REQUEST_METHOD_GET, "parent",
+		parent_handler, HTTP_ROUTE_AUTH_TYPE_BEARER
+	)
+
+	# GET /parent/child
+	auth_route = http_create_child_route (
+		parent_route, REQUEST_METHOD_GET, "child",
+		child_handler, HTTP_ROUTE_AUTH_TYPE_BEARER
+	)
+
+	cerver_start (web_service)
 
 if __name__ == "__main__":
-	cerver_initialize (end, True)
+	signal.signal (signal.SIGINT, end)
+	signal.signal (signal.SIGTERM, end)
+	signal.signal (signal.SIGPIPE, signal.SIG_IGN)
 
-	start()
+	cerver_init ()
+
+	cerver_version_print_full ()
+
+	pycerver_version_print_full ()
+
+	start ()
